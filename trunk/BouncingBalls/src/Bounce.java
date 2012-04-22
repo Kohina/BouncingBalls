e6f9e7164f1379a3b3de178b755ac40869fcb46a
hansson.marika@gmail.com <hansson.marika@gmail.com@5ade5902-9c97-a574-0079-5fcb5de56f3c>
2012-04-22 13:57:03 +0000

    fixat så bollarna inte fastnar i väggarna

--- trunk/BouncingBalls/src/Bounce.java
+++ trunk/BouncingBalls/src/Bounce.java
@@ -38,48 +38,6 @@ public class Bounce extends Animation {
 		// g.fillRect(0,0,d.width,d.height); // slower?
 		g.fillOval(pixelX1 - radius, pixelY1 - radius, radius * 2, radius * 2);
 		g.fillOval(pixelX2 - radius, pixelY2 - radius, radius * 2, radius * 2);
-
-		if (pixelX1 > d.width-radius) {
-			pixelX1 = d.width-radius;
-			Vx1 = -Vx1;
-		}
-		if (pixelX1 < radius) {
-			pixelX1 = 0+radius;
-			Vx1 = -Vx1;
-		}
-		if(pixelY1 > d.height-radius){
-			pixelY1 = d.height-radius;
-			Vy1 = -Vy1;
-		}
-		if (pixelY1 < radius) {
-			pixelY1 = 0+radius;
-			Vy1 = -Vy1;
-			// TODO: Maybe change here?
-			/*if (Vy1 < 5 && Vy1 > -5) {
-				Vy1 = grav * deltaT;
-			}*/
-		}
-		
-		if(pixelX2 > d.width-radius){
-			pixelX2 = d.width-radius;
-			Vx2 = -Vx2;
-		}
-		if (pixelX2 < radius) {
-			pixelX2 = 0+radius;
-			Vx2 = -Vx2;
-		}
-		if(pixelY2 > d.height-radius){
-			pixelX2 = d.height-radius;
-			Vx2 = -Vx2;
-		}
-		if (pixelY2 < radius) {
-			pixelY2 = 0+radius;
-			Vy2 = -Vy2;
-			// TODO: Maybe change here?
-			/*if (Vy2 < 5 && Vy2 > -5) {
-				Vy2 = grav * deltaT;
-			}*/
-		}
 		
 		if(Math.abs(Math.sqrt(Math.pow(pixelX1-pixelX2, 2)+Math.pow(pixelY1-pixelY2, 2))) < 2*radius) {
 			int dX = pixelX1 - pixelX2; //Avstånd mellan bollarna i x-led
@@ -115,18 +73,52 @@ public class Bounce extends Animation {
 		
 		Vy1 -= grav * deltaT;
 		Vy2 -= grav * deltaT;
-
+		
 		X1 += Vx1 * deltaT;
 		Y1 += Vy1 * deltaT;
 
 		X2 += Vx2 * deltaT;
 		Y2 += Vy2 * deltaT;
-
+		
 		pixelX1 = (int) (pixelsPerMeter * X1);
 		pixelY1 = (int) (pixelsPerMeter * Y1);
 		pixelX2 = (int) (pixelsPerMeter * X2);
 		pixelY2 = (int) (pixelsPerMeter * Y2);
-
+		
+		if (pixelX1 > d.width-radius) {
+			pixelX1 = d.width-radius;
+			Vx1 = -Vx1;
+		}
+		if (pixelX1 < radius) {
+			pixelX1 = 0+radius;
+			Vx1 = -Vx1;
+		}
+		if(pixelY1 > d.height-radius){
+			pixelY1 = d.height-radius;
+			Vy1 = -Vy1;
+		}
+		if (pixelY1 < radius) {
+			pixelY1 = 0+radius;
+			Vy1 = -Vy1;
+		}
+		
+		if(pixelX2 > d.width-radius){
+			pixelX2 = d.width-radius;
+			Vx2 = -Vx2;
+		}
+		if (pixelX2 < radius) {
+			pixelX2 = 0+radius;
+			Vx2 = -Vx2;
+		}
+		if(pixelY2 > d.height-radius){
+			pixelY2 = d.height-radius;
+			Vy2 = -Vy2;
+		}
+		if (pixelY2 < radius) {
+			pixelY2 = 0+radius;
+			Vy2 = -Vy2;
+		}
+		
 		g.setColor(color);
 		g.fillOval(pixelX1 - radius, pixelY1 - radius, radius * 2, radius * 2);
 		g.setColor(Color.BLUE);
