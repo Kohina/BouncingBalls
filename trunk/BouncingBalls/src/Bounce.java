8e3d1a80833a2a3ad26dc22421c992d41489e94e
ninja.malm@gmail.com <ninja.malm@gmail.com@5ade5902-9c97-a574-0079-5fcb5de56f3c>
2012-05-09 09:02:11 +0000

    Used atan2 instead of atan.

--- trunk/BouncingBalls/src/Bounce.java
+++ trunk/BouncingBalls/src/Bounce.java
@@ -31,8 +31,8 @@ public class Bounce extends Animation {
 		pixelsPerMeter = 40;
 		grav = 9.8;
 
-		balls[0] = new Ball(1, 3, 20, 7, 0.05, 0.05, Color.red);
-		balls[1] = new Ball(1, 1, 20, 7, 5, 4, Color.blue);		
+		balls[0] = new Ball(1, 3, 20, 3, 0.05, 0.05, Color.red);
+		balls[1] = new Ball(1, 1, 10, 7, 5, 4, Color.blue);		
 	}
 
 	protected void paintAnimator(Graphics g) {
@@ -70,8 +70,8 @@ public class Bounce extends Animation {
 		g.fillOval(pixelX2 - balls[1].getRadius(), d.height - pixelY2
 				- balls[1].getRadius(), balls[1].getRadius() * 2,
 				balls[1].getRadius() * 2);
-		g.drawLine(pixelX1, d.height -pixelY1, pixelX1+(int)(balls[0].getVx()*10), d.height -(int)(pixelY1+balls[0].getVy()*10));
-		g.drawLine(pixelX2, d.height -pixelY2, pixelX2+(int)(balls[1].getVx()*10), d.height -(int)(pixelY2+balls[1].getVy()*10));
+		//g.drawLine(pixelX1, d.height -pixelY1, pixelX1+(int)(balls[0].getVx()*10), d.height -(int)(pixelY1+balls[0].getVy()*10));
+		//g.drawLine(pixelX2, d.height -pixelY2, pixelX2+(int)(balls[1].getVx()*10), d.height -(int)(pixelY2+balls[1].getVy()*10));
 	}
 	
 	private void paintBall(Ball ball, Graphics g) {
@@ -84,7 +84,7 @@ public class Bounce extends Animation {
 				ball.getRadius() * 2,
 				ball.getRadius() * 2);
 		g.setColor(ball.getColor().darker());
-		g.drawLine(pixelX, d.height -pixelY, pixelX+(int)(ball.getVx()*10), d.height -(int)(pixelY+ball.getVy()*10));
+		//g.drawLine(pixelX, d.height -pixelY, pixelX+(int)(ball.getVx()*10), d.height -(int)(pixelY+ball.getVy()*10));
 	}
 	
 	private void handleWallCollision(Ball ball) {
@@ -105,7 +105,7 @@ public class Bounce extends Animation {
 			ball.setVelocity(ball.getVx(), -ball.getVy());
 		}
 		
-		//fixOverlap(ball);
+		fixOverlap(ball);
 	}
 	
 	private boolean collisionHandled = false; 
@@ -123,12 +123,12 @@ public class Bounce extends Animation {
 		
 		if (Math.sqrt(dX*dX + dY*dY) <= ball1.getRadius() + ball2.getRadius()) {
 			if (!collisionHandled) {
-			double a = Math.atan((double) dY / dX); // vinkeln som bollarna tr�ffas i
+			double a = Math.atan2((double) dY, (double) dX); // vinkeln som bollarna tr�ffas i
 
 			double V1 = Math.sqrt(Math.pow(ball1.getVx(), 2) + Math.pow(ball1.getVy(), 2)); //Total velocity for ball1
-			double aV1 = Math.atan(ball1.getVy() / ball1.getVx()); //Velocity angel ball1
+			double aV1 = Math.atan2(ball1.getVy(), ball1.getVx()); //Velocity angel ball1
 			double V2 = Math.sqrt(Math.pow(ball2.getVx(), 2) + Math.pow(ball2.getVy(), 2)); //Total velocity for ball2
-			double aV2 = Math.atan(ball2.getVy() / ball2.getVx()); //Velocity angel ball2
+			double aV2 = Math.atan2(ball2.getVy(), ball2.getVx()); //Velocity angel ball2
 			double a1 = aV1 - a; //Angel in the new coordinate-system for ball1
 			double a2 = aV2 - a; //Angel in the new coordinate-system for ball2
 
@@ -136,33 +136,33 @@ public class Bounce extends Animation {
 			double Vg1 = V1 * Math.sin(a1); //velocity along the line perpendicular to the line between the centers of the balls (g-axis)
 			double Vf2 = V2 * Math.cos(a2);
 			double Vg2 = V2 * Math.sin(a2);
-			
+			/*
 			System.out.println("a: " + a);
 			System.out.println("U1: " + V1 + ", " + aV1 + " radians");
 			System.out.println("U2: " + V2 + ", " + aV2 + " radians");
-
+*/
 			int m1 = ball1.getMass();
 			int m2 = ball2.getMass();
 			double Uf1 = Vf1;
 			double Uf2 = Vf2;
 			Vf1 = (m1 * Uf1 + 2 * m2 * Uf2 - m2 * Uf1) / (m1 + m2);
 			Vf2 = -Uf2 + Uf1 + Vf1;
-			
+			/*
 			System.out.println("Uf1: " + Uf1 + ", Vf1: " + Vf1);
 			System.out.println("Uf2: " + Uf2 + ", Vf2: " + Vf2);
-
-			double b1 = Math.atan(Vg1 / Vf1); //angle between the f-axis and the new velocity
-			double b2 = Math.atan(Vg2 / Vf2);
+*/
+			double b1 = Math.atan2(Vg1, Vf1); //angle between the f-axis and the new velocity
+			double b2 = Math.atan2(Vg2, Vf2);
 			V1 = Math.sqrt(Math.pow(Vf1, 2) + Math.pow(Vg1, 2)); //new total velocity for ball1
 			V2 = Math.sqrt(Math.pow(Vf2, 2) + Math.pow(Vg2, 2)); //new total velocity for ball2
 
 			ball1.setVelocity(V1 * Math.cos(a + b1), V1 * Math.sin(a + b1)); //convert velocity back to x, y coordinates and set
 			ball2.setVelocity(V2 * Math.cos(a + b2), V2 * Math.sin(a + b2));
-			
+			/*
 			System.out.println("V1: " + V1 + ", " + (b1+a) + " radians");
 			System.out.println("V2: " + V2 + ", " + (b2+a) + " radians");
 			System.out.println("-----------------------------------------------");
-			
+			*/
 			//fixOverlap(balls[0], balls[1]);
 			collisionHandled = true;
 			}
@@ -270,7 +270,7 @@ public class Bounce extends Animation {
 	}
 	
 	private void applyGravity(Ball ball) {
-		//ball.setVelocity(ball.getVx(), ball.getVy() - grav * deltaT);
+		ball.setVelocity(ball.getVx(), ball.getVy() - grav * deltaT);
 	}
 	
 	private void applyPositionChange(Ball ball) {
